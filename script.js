6ef17494c62d6366b110230ce57de31fe1c5255a
Gokul Krishnan <121749145+krish19098@users.noreply.github.com>
2023-10-31 12:12:37 +0530

    Add files via upload
    HERE i place the CSS , HTML, JS code
    HTML
    <!DOCTYPE html>
    <html>
    
    <head>
        <meta charset="utf-8">
        <meta name="viewport" content="width=device-width, initial-scale=1">
        <title>Random Dog Image</title>
        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
        <link rel="stylesheet" href="styles.css">
    </head>
    
    <body>
        <div class="container text-center mt-5">
            <h1>Random Dog Image</h1>
            <div class="image-container mt-4">
                <img src="" alt="Random Dog Image" id="dog-image" class="img-fluid">
            </div>
            <button id="fetch-button" class="btn btn-primary mt-4">Fetch Random Dog Image</button>
        </div>
        <script src="script.js"></script>
    </body>
    
    </html>
    
    CSS
    body {
        background-color: #f5f5f5;
    }
    .image-container {
        max-width: 500px;
        margin: 0 auto;
    }
    /* Button hover effect */
    .btn-primary:hover {
        background-color: #0056b3;
    }
    
    JS
    // Function - a random dog image
    function getRandomDogImage() {
        return fetch('https://dog.ceo/api/breeds/image/random')
            .then(response => {
                if (!response.ok) {
                    throw new Error('Network response was not ok');
                }
                return response.json();
            });
    }
    
    // Function - imagesource
    function setImageSource(imageUrl) {
        const dogImage = document.getElementById('dog-image');
        dogImage.src = imageUrl;
    }
    
    // Function - click event
    function handleButtonClick() {
        getRandomDogImage()
            .then(data => {
                const imageUrl = data.message;
                setImageSource(imageUrl);
            })
            .catch(error => {
                console.error('Error fetching random dog image:', error);
            });
    }
    
    // click event listener to the button
    const fetchButton = document.getElementById('fetch-button');
    fetchButton.addEventListener('click', handleButtonClick);

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-    const fetchDataButton = document.getElementById("fetchData");
-    const bookInfo = document.querySelector(".book-info");
+// Function - a random dog image
+function getRandomDogImage() {
+    return fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        });
+}
 
-    fetchDataButton.addEventListener("click", () => {
-        fetchBookData()
-            .then(displayBookInfo)
-            .catch((error) => console.error(error));
-    });
+// Function - imagesource
+function setImageSource(imageUrl) {
+    const dogImage = document.getElementById('dog-image');
+    dogImage.src = imageUrl;
+}
 
-    function fetchBookData() {
-        return fetch("https://wiki.anidb.net/HTTP_API_Definition")
-            .then((response) => response.json());
-    }
+// Function - click event
+function handleButtonClick() {
+    getRandomDogImage()
+        .then(data => {
+            const imageUrl = data.message;
+            setImageSource(imageUrl);
+        })
+        .catch(error => {
+            console.error('Error fetching random dog image:', error);
+        });
+}
 
-    function displayBookInfo(data) {
-        if (data && data.length > 0) {
-            const bookList = document.createElement("ul");
-            data.forEach((book) => {
-                const listItem = document.createElement("li");
-                listItem.textContent = `Title: ${book.title}, Author: ${book.author}`;
-                bookList.appendChild(listItem);
-            });
-            bookInfo.innerHTML = "";
-            bookInfo.appendChild(bookList);
-        } else {
-            bookInfo.textContent = "No book information available.";
-        }
-    }
-});
+// click event listener to the button
+const fetchButton = document.getElementById('fetch-button');
+fetchButton.addEventListener('click', handleButtonClick);
